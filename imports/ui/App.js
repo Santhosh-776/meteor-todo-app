de5349e9183c42cb158783151fef5c93ede0a5fb
Santhosh <santhoshnadarajan.work@gmail.com>
2026-06-17 15:07:16 +0530

    feat: display list of tasks and toggle task checkbox status

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,2 +1,10 @@
 import { Template } from 'meteor/templating';
+import { TasksCollection } from '../api/tasksCollection';
 import './App.html';
+import './Task.js';
+
+Template.app.helpers({
+  tasks() {
+    return TasksCollection.find({}, { sort: { position: 1 } });
+  },
+});
---
+++ imports/ui/Task.js
@@ -0,0 +1,12 @@
+import { Template } from 'meteor/templating';
+import { TasksCollection } from '../api/tasksCollection';
+import './Task.html';
+
+Template.Task.events({
+  async 'change .toggle-checked'(event) {
+    // Toggle checked status in the database asynchronously
+    await TasksCollection.updateAsync(this._id, {
+      $set: { checked: event.target.checked },
+    });
+  },
+});
