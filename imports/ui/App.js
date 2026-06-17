79f40c7e472dad4db6e73f1b2f0d7f9b4717a5c8
Santhosh <santhoshnadarajan.work@gmail.com>
2026-06-17 15:13:28 +0530

    feat: integrate sortablejs to allow dragging and reordering

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,11 +1,52 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import Sortable from 'sortablejs';
 import { TasksCollection } from '../api/tasksCollection';
 import './App.html';
 import './Task.js';
 
 Template.app.onCreated(function () {
   this.categoryFilter = new ReactiveVar('');
+});
+
+Template.app.onRendered(function () {
+  const list = this.find('#tasks-list');
+  Sortable.create(list, {
+    animation: 150,
+    handle: '.task-content', // Only drag by the content container to avoid interfering with delete button
+    async onEnd(evt) {
+      const movedId = evt.item.dataset.id;
+      const nextSibling = evt.item.nextElementSibling;
+      const nextId = nextSibling ? nextSibling.dataset.id : null;
+
+      const allTasks = await TasksCollection.find({}, { sort: { position: 1 } }).fetchAsync();
+      const movedTaskIndex = allTasks.findIndex(t => t._id === movedId);
+      if (movedTaskIndex === -1) return;
+
+      const [movedTask] = allTasks.splice(movedTaskIndex, 1);
+
+      if (nextId) {
+        const nextIndex = allTasks.findIndex(t => t._id === nextId);
+        allTasks.splice(nextIndex, 0, movedTask);
+      } else {
+        const prevSibling = evt.item.previousElementSibling;
+        const prevId = prevSibling ? prevSibling.dataset.id : null;
+        if (prevId) {
+          const prevIndex = allTasks.findIndex(t => t._id === prevId);
+          allTasks.splice(prevIndex + 1, 0, movedTask);
+        } else {
+          allTasks.push(movedTask);
+        }
+      }
+
+      // Update positions in the database
+      allTasks.forEach(async (task, index) => {
+        await TasksCollection.updateAsync(task._id, {
+          $set: { position: index }
+        });
+      });
+    }
+  });
 });
 
 Template.app.helpers({
